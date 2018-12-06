9b1d6344f09b9ad3eb7665bccfc891536dcfafdf
박진수 <coolgary@kolon.com>
2018-12-06 17:22:55 +0900

    React Components Task 1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+  CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props); 
+    }
+
+    render() {
+        console.log(this.props.dish)
+        if(this.props.dish != null){
+          return (
+            <div className="row">
+              <div  className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                    <CardBody>
+                      <CardTitle>{this.props.dish.name}</CardTitle>                      
+                    </CardBody>
+                </Card>
+              </div>
+            </div>
+          );
+
+        }else{
+          return <div/>
+        }
+        
+    }
+}
+
+export default DishDetail;
