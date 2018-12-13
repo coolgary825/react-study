01351ba0e42bd31e42734fc4ecc75935cfff2fb4
박진수 <coolgary@kolon.com>
2018-12-13 20:32:04 +0900

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,8 @@ import DishDetail from './DishdetailComponent';
 import {DISHES} from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
@@ -21,17 +23,24 @@ class Main extends Component {
     
 
     render() {
+        const HomePage = () => {
+            return(
+                <Home />
+            )
+        }
+
+
         return (
             
-            <div>
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                </div>
-                </Navbar>
+            <div>               
                 <Header />
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} /> } />
+                    <Redirect to="/home" />
+                </Switch>
+
                 <Footer />
             </div>
         );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -16,6 +16,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
     };
 
     const Menu = (props) => {
+        
         const menu = props.dishes.map((dish) => {
             return (
               <div  className="col-12 col-md-5 m-1" key={dish.id}>
