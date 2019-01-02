6589c0a1f5eca15d4f28125c397ccc1dbb2ef481
박진수 <coolgary@kolon.com>
2019-01-02 17:31:19 +0900

    Intro. to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -81,4 +81,4 @@ class Main extends Component {
     }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux';
+import { createStore } from 'redux';
 import { Reducer, initialState } from './reducer'
 
 export const ConfigureStore = () => {
